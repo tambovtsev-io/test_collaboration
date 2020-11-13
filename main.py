f634e4b4c92b40ab7d6594c226846bc3c1d36e78
Ros Kuz <kuzin.rf@phystech.edu>
2020-11-13 13:28:21 +0700

    Added Kuzin feature

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ class OurGroup():
     
     def kolganov(self):
         print("I am kolganov")
-
-    def povolotskii(self):
-        print("Let's talk about bromine")
+        
+    def kuzin(self):
+        print("I am kuzin")
