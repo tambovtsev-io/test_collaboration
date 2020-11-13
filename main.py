c99a5bcbf650abe44adaaf20bcde044baba37e1d
Alexandr Razin <71607906+Larkiskek@users.noreply.github.com>
2020-11-13 09:33:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 class OurGroup():
     def __init__(self):
         pass
-
+    
     def kolganov(self):
         print("I am kolganov")
 
-
-    def tambovtsev(self):
-        print("dratuti")
-
     def povolotskii(self):
         print("Let's talk about bromine")
-
+    
+    def razin(self):
+        print("— Allo, eto prachechnaya? — Da")
